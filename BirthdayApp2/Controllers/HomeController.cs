28f2dd276201a060199fd6c61552da3d513fc609
gokhanyzc <gokhanyzc9797@gmail.com>
2019-11-18 17:13:03 +0300

    project

---
+++ BirthdayApp2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using BirthdayApp2.Models; //gerekli kütüphane.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DavetiyeFormu()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        public ActionResult DavetiyeFormu(DavetiyeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                VeriTabani.Add(model);
+                return View("Thanks", model);
+
+            }
+
+            else
+            {
+                return View(model);
+            }
+
+        }
+
+        public ActionResult Katilanlar()
+        {
+            Thread.Sleep(2000);
+            //KATILANLARI LİSTELEDİ.
+            return PartialView(VeriTabani.Liste.Where(i=>i.KatilmaDurumu == true));
+
+        }
+
+        public ActionResult KatilanAra(string q)
+        {
+            Thread.Sleep(2000);
+            return PartialView(VeriTabani.Liste.Where(i => i.Ad.Contains(q))); //ad içerisinde arama yapar.
+        }
+
+    }
+}
---
+++ BirthdayApp2/Models/VeriTabani.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayApp2.Models
+{
+    public class VeriTabani
+    {
+        private static Dictionary<string, DavetiyeModel> _liste;
+
+        //SANAL VERİTABANI
+        static VeriTabani() //constructor
+        {
+            _liste = new Dictionary<string, DavetiyeModel>();
+            _liste.Add("Hasan", new DavetiyeModel
+            {
+                Ad = "Hasan",
+                Eposta = "hasan61@gmail.com",
+                KatilmaDurumu = true
+            });
+
+            _liste.Add("Gökhan", new DavetiyeModel
+            {
+                Ad = "Gökhan",
+                Eposta = "gokhanyzc9797@gmail.com",
+                KatilmaDurumu = true
+            });
+
+            _liste.Add("Melike", new DavetiyeModel
+            {
+                Ad = "Melike",
+                Eposta = "melikevarlik@gmail.com",
+                KatilmaDurumu = false
+
+            });
+
+        }
+            public static void Add(DavetiyeModel model)
+            {
+                string key = model.Ad.ToLower(); //Tolower, string'e atanan metnin tüm karakterlerini küçük harfe çevirmemizi sağlar.
+                if (_liste.ContainsKey(key))
+                {
+                    _liste[key] = model;
+
+                }
+
+                else
+                {
+                    _liste.Add(key, model);
+                }
+            }
+
+            public static IEnumerable<DavetiyeModel> Liste
+        {
+            get { return _liste.Values; }
+        }
+    }
+}
+
